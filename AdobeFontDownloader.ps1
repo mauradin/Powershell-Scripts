5d7ab99a0c139e5b6b6ba5ecfae3584dbc520d94
Mauradin <jmc4801@gmail.com>
2025-09-10 10:30:39 -0500

    Added

---
+++ AdobeFontDownloader.ps1
@@ -0,0 +1,25 @@
+$FontsDir = "C:\Program Files\Adobe\Acrobat DC\Resource\Font"
+$ZipFilePath = "C:\Program Files\Adobe\Acrobat DC\Resource\Font\ETCOMFonts.zip"
+$DownloadUrl = "https://acumentechnology-my.sharepoint.com/:u:/p/jcampanella/EUBWjry6wcREsFIqJoX8Op4Bwe_DNXVDNjYYyxFwmTZz7g?download=1 "
+
+if (-not (Test-Path $FontsDir)) {
+    New-Item -Path $FontsDir -ItemType Directory
+} else {
+}
+
+if (Test-Path $ZipFilePath) {
+} else {
+    
+    try {
+        Invoke-WebRequest -Uri $DownloadUrl -OutFile $ZipFilePath -TimeoutSec 300
+    } catch {
+        exit 1
+    }
+}
+
+Expand-Archive -Path $ZipFilePath -DestinationPath $FontsDir -Force
+Start-Sleep -Seconds 10
+if (Test-Path $ZipFilePath) {
+    Remove-Item -Path $ZipFilePath -Force
+} else {
+}
